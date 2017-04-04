3f7b359c1991651274e4a0fb3a0214e7439b651e
Glen Sawyer <glensawyer@hotmail.com>
2017-04-04 16:41:48 -0600

    Initial functionality complete

--- src/BYUJWT.php
+++ src/BYUJWT.php
@@ -1,23 +1,26 @@
 <?php
-// Copyright 2017 Brigham Young University
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/* Copyright 2017 Brigham Young University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 
 namespace BYU\JWT;
 
+use Exception;
 use Firebase\JWT\JWT;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use phpseclib\File\X509;
 
 /**
  * Provides helpful functions to retrieve a specified BYU .well-known URL and verify BYU signed JWTs
@@ -27,19 +30,24 @@ use GuzzleHttp\Exception\RequestException;
 class BYUJWT
 {
     public static $wellKnownHost = 'https://api.byu.edu';
-    public static $cacheWellKnowns = false;
-    private static $_cache = [];
+    public static $useCache = true;
+    public static $lastException;
+    protected static $_cache = [];
+    protected static $_client;
 
     const BYU_JWT_HEADER_CURRENT = "X-JWT-Assertion";
     const BYU_JWT_HEADER_ORIGINAL = "X-JWT-Assertion-Original";
 
     /**
      * Clear cache, reset variables
+     *
+     * @return void
      */
     public static function reset()
     {
         static::$wellKnownHost = 'https://api.byu.edu';
-        static::$cacheWellKnowns = false;
+        static::$useCache = true;
+        static::$lastException = null;
         static::$_cache = [];
     }
 
@@ -51,50 +59,126 @@ class BYUJWT
      */
     public static function getWellKnown()
     {
-        if(static::$cacheWellKnowns && array_key_exists(static::$wellKnownHost, static::$_cache)) {
-            return static::$_cache[static::$wellKnownHost];
+        if(static::$useCache) {
+            if (array_key_exists(static::$wellKnownHost, static::$_cache) && array_key_exists('well-known', static::$_cache[static::$wellKnownHost])) {
+                return static::$_cache[static::$wellKnownHost]['well-known'];
+            }
+            static::$_cache[static::$wellKnownHost]['well-known'] = null;
         }
-        static::$_cache[static::$wellKnownHost] = null;
 
         try {
-            $client = new Client();
-            $response = $client->get(trim(static::$wellKnownHost, '/') . '/.well-known/openid-configuration');
+            $response = static::client()->get(trim(static::$wellKnownHost, '/') . '/.well-known/openid-configuration');
         } catch (RequestException $e) {
             return null;
         }
 
-        static::$_cache[static::$wellKnownHost] = @json_decode($response->getBody());
+        $output = @json_decode($response->getBody());
+        if (static::$useCache) {
+            static::$_cache[static::$wellKnownHost]['well-known'] = $output;
+        }
 
-        return static::$_cache[static::$wellKnownHost];
+        return $output;
     }
 
+    /**
+     * Override the base "well known URL"
+     *
+     * @return void
+     */
     public static function setWellKnownHost($host)
     {
         static::$wellKnownHost = $host;
     }
 
+    /**
+     * Get the public key of the current well-known URL
+     *
+     * @return string
+     */
     public static function getPublicKey()
     {
-        //TODO
+        if(static::$useCache) {
+            if (array_key_exists(static::$wellKnownHost, static::$_cache) && array_key_exists('public-key', static::$_cache[static::$wellKnownHost])) {
+                return static::$_cache[static::$wellKnownHost]['public-key'];
+            }
+            static::$_cache[static::$wellKnownHost]['public-key'] = null;
+        }
+
+        $wellKnown = static::getWellKnown();
+        if (empty($wellKnown->jwks_uri)) {
+            return null;
+        }
+
+        try {
+            $response = static::client()->get($wellKnown->jwks_uri);
+        } catch (RequestException $e) {
+            return null;
+        }
+
+        $jwks = json_decode($response->getBody());
+        if (empty($jwks->keys[0]->x5c[0])) {
+            return null;
+        }
+
+        $X509 = new X509();
+        if (!$X509->loadX509($jwks->keys[0]->x5c[0])) {
+            return null;
+        }
+
+        $key = (string)$X509->getPublicKey();
+        if (static::$useCache) {
+            static::$_cache[static::$wellKnownHost]['public-key'] = $key;
+        }
+        return $key;
     }
 
-    public static function verifyJWT($jwt)
+    /**
+     * Check if a JWT is valid
+     *
+     * @param string JWT
+     *
+     * @return boolean true if $jwt is a valid JWT, false if not
+     */
+    public static function validateJWT($jwt)
     {
-        //TODO
+        try {
+            $decoded = static::decode($jwt);
+            return !empty($decoded);
+        } catch (Exception $e) {
+            //For simple true/false validation we don't throw exceptions; just return false
+            //but store exception in case further details are wanted
+            static::$lastException = $e;
+            return false;
+        }
     }
 
-    public static function jwtDecoded($jwt)
+    /**
+     * Decode a JWT
+     *
+     * @param string JWT
+     *
+     * @return object decoded JWT
+     *
+     * @throws Exception Various exceptions for various problems with JWT (see Firebase\JWT\JWT::decode for details)
+     */
+    public static function decode($jwt)
     {
-      $jwt = JWT::encode($token, $key);
-      $decoded = JWT::decode($jwt, $key, array('HS256'));
-      
-      print_r($decoded);
-       
-      /*
-       NOTE: This will now be an object instead of an associative array. To get
-       an associative array, you will need to cast it as such:
-       */
-       $decoded_array = (array) $decoded;
-       return $decoded_array;
+        $key = static::getPublicKey();
+        $decoded = JWT::decode($jwt, $key, ['HS256','RS256','HS512','HS384']);
+        if (empty($decoded->exp)) {
+            //Firebase\JWT\JWT::decode does not throw an error if "exp" does not exist,
+            //although it does throw an error if it exists but has already passed
+            //For BYU we want to ensure that it does exist, as well as being valid
+            throw new NoExpirationException('No expiration in JWT');
+        }
+        return $decoded;
+    }
+
+    protected static function client()
+    {
+        if (empty(static::$_client)) {
+            static::$_client = new Client();
+        }
+        return static::$_client;
     }
 }
--- tests/BYUJWTTest.php
+++ tests/BYUJWTTest.php
@@ -1,6 +1,23 @@
 <?php
+/* Copyright 2017 Brigham Young University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+namespace BYU\JWT\Test;
 
 use BYU\JWT\BYUJWT;
+use Firebase\JWT\JWT;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use PHPUnit\Framework\TestCase;
@@ -12,7 +29,9 @@ final class BYUJWTTest extends TestCase
 {
 
     // Global valid jwt for use in tests against a valid JWT
-    protected $validJWT = "";
+    protected static $validJWT = "";
+    // credentials used to generate valid JWT above
+    protected static $credentials;
 
     /*
      * The test suite requires the wso2-test-credentials.json file to be present
@@ -24,13 +43,13 @@ final class BYUJWTTest extends TestCase
         $fileLocation = getenv("WSO2_CRED_LOC") ?: dirname(dirname(__FILE__)) . '/wso2-test-credentials.json';
 
         $credentialsJSON = file_get_contents($fileLocation);
-        $credentials = json_decode($credentialsJSON);
+        static::$credentials = json_decode($credentialsJSON);
 
         $client = new Client();
         try {
             $response = $client->post('https://api.byu.edu/token', [
                 'body' => 'grant_type=client_credentials',
-                'auth' => [$credentials->client_id, $credentials->client_secret]]);
+                'auth' => [static::$credentials->client_id, static::$credentials->client_secret]]);
         } catch (RequestException $e) {
             return null;
         }
@@ -44,7 +63,7 @@ final class BYUJWTTest extends TestCase
             return null;
         }
         $echoBody = json_decode($echoResponse->getBody(), true);
-        $this->validJWT = $echoBody["X-Jwt-Assertion"][0];
+        static::$validJWT = $echoBody['Headers']["X-Jwt-Assertion"][0];
     }
 
     public function setUp() {
@@ -58,6 +77,13 @@ final class BYUJWTTest extends TestCase
         $alternateWellKnownHost = "http://fake.com";
         BYUJWT::setWellKnownHost($alternateWellKnownHost);
         $this->assertEquals($alternateWellKnownHost, BYUJWT::$wellKnownHost);
+        BYUJWT::setWellKnownHost($existingHost);
+    }
+
+    public function testGetPublicKey()
+    {
+        $key = BYUJWT::getPublicKey();
+        $this->assertNotEmpty($key);
     }
 
     public function testGetWellKnown()
@@ -72,7 +98,8 @@ final class BYUJWTTest extends TestCase
         //Using assertSame instead of assertEquals in this case, because
         //we want to ensure actual boolean "false" instead of "falsy" values
         //like void, null, 0, etc.
-        $this->assertSame(false, BYUJWT::verifyJWT("Um, some bad JWT here"));
+        $this->assertSame(false, BYUJWT::validateJWT("Blah blah this is not at all a valid JWT"));
+        $this->assertInstanceOf('Exception', BYUJWT::$lastException);
     }
 
     public function testExpiredJWTReturnError()
@@ -80,13 +107,19 @@ final class BYUJWTTest extends TestCase
         // Expired JWT for a bot user
         $expiredJWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlpUUm1NMk5tWkRabFlUZG1aRFJqWVdJME1tTXpZamd4WWpNd1lXUXhNems0TnpFd09EVmxNdyJ9.eyJpc3MiOiJodHRwczovL2FwaS5ieXUuZWR1IiwiZXhwIjoxNDg5NzY4MTczLCJodHRwOi8vd3NvMi5vcmcvY2xhaW1zL3N1YnNjcmliZXIiOiJCWVUvYWRkZHJvcCIsImh0dHA6Ly93c28yLm9yZy9jbGFpbXMvYXBwbGljYXRpb25pZCI6IjIwODUiLCJodHRwOi8vd3NvMi5vcmcvY2xhaW1zL2FwcGxpY2F0aW9ubmFtZSI6IkRlZmF1bHRBcHBsaWNhdGlvbiIsImh0dHA6Ly93c28yLm9yZy9jbGFpbXMvYXBwbGljYXRpb250aWVyIjoiVW5saW1pdGVkIiwiaHR0cDovL3dzbzIub3JnL2NsYWltcy9hcGljb250ZXh0IjoiL2VjaG8vdjEiLCJodHRwOi8vd3NvMi5vcmcvY2xhaW1zL3ZlcnNpb24iOiJ2MSIsImh0dHA6Ly93c28yLm9yZy9jbGFpbXMvdGllciI6IkJyb256ZSIsImh0dHA6Ly93c28yLm9yZy9jbGFpbXMva2V5dHlwZSI6IlBST0RVQ1RJT04iLCJodHRwOi8vd3NvMi5vcmcvY2xhaW1zL3VzZXJ0eXBlIjoiQVBQTElDQVRJT04iLCJodHRwOi8vd3NvMi5vcmcvY2xhaW1zL2VuZHVzZXIiOiJhZGRkcm9wQGNhcmJvbi5zdXBlciIsImh0dHA6Ly93c28yLm9yZy9jbGFpbXMvZW5kdXNlclRlbmFudElkIjoiLTEyMzQiLCJodHRwOi8vd3NvMi5vcmcvY2xhaW1zL2NsaWVudF9pZCI6IjVnekxqTVVjeDdxdXQzTXVTZjl4cjhHVjJCQWEiLCJodHRwOi8vYnl1LmVkdS9jbGFpbXMvY2xpZW50X3Jlc3Rfb2ZfbmFtZSI6IkFkZCIsImh0dHA6Ly9ieXUuZWR1L2NsYWltcy9jbGllbnRfcGVyc29uX2lkIjoiMzc3MjI4MDYyIiwiaHR0cDovL2J5dS5lZHUvY2xhaW1zL2NsaWVudF9zb3J0X25hbWUiOiJEcm9wLCBBZGQiLCJodHRwOi8vYnl1LmVkdS9jbGFpbXMvY2xpZW50X2NsYWltX3NvdXJjZSI6IkNMSUVOVF9TVUJTQ1JJQkVSIiwiaHR0cDovL2J5dS5lZHUvY2xhaW1zL2NsaWVudF9uZXRfaWQiOiJhZGRkcm9wIiwiaHR0cDovL2J5dS5lZHUvY2xhaW1zL2NsaWVudF9zdWJzY3JpYmVyX25ldF9pZCI6ImFkZGRyb3AiLCJodHRwOi8vYnl1LmVkdS9jbGFpbXMvY2xpZW50X25hbWVfc3VmZml4IjoiICIsImh0dHA6Ly9ieXUuZWR1L2NsYWltcy9jbGllbnRfc3VybmFtZSI6IkRyb3AiLCJodHRwOi8vYnl1LmVkdS9jbGFpbXMvY2xpZW50X3N1cm5hbWVfcG9zaXRpb24iOiJMIiwiaHR0cDovL2J5dS5lZHUvY2xhaW1zL2NsaWVudF9uYW1lX3ByZWZpeCI6IiAiLCJodHRwOi8vYnl1LmVkdS9jbGFpbXMvY2xpZW50X2J5dV9pZCI6IjY0OTAxOTk2NSIsImh0dHA6Ly9ieXUuZWR1L2NsYWltcy9jbGllbnRfcHJlZmVycmVkX2ZpcnN0X25hbWUiOiJBZGQifQ.luAysmbldL0Hn1PO1TeHSba79tiVwnaEFLPMknsX5xegqExrJXs_FEge8R7PWj-KnjCMZt1QZbUeDz9boBWednXORSIHOk8TIZzM1hc3kM883sQXRbe9hiTfnoWf0zN2i9B6LBV1vqSFgJu7-PP_kAk0E1kfi7TcbWjecYc9H6vBijHrguh-WvwLuHg5qjC7en-FWcoYO_yM1oWvNajrUUdFbW6WC7lgkvnAPqreNlCuRA23uk45incuMNyFtldlMVOtAsqxRFgpydnujmnuP-l8gU2L1zFXdOkj-gTmq4v-sMCS2crrYW4MIVy5ObqlsQMOisjqturQLuxLo9fYpQ";
 
-        $this->assertSame(false, BYUJWT::verifyJWT($expiredJWT, "https://api.byu.edu"));
+        $this->assertSame(false, BYUJWT::validateJWT($expiredJWT));
+        $this->assertInstanceOf('Firebase\JWT\ExpiredException', BYUJWT::$lastException);
     }
 
     public function testJWTWithNoExpiration()
     {
-        //Verify false when JWT Expiration does not exist
-        $this->assertSame(false, BYUJWT::verifyJWT("Seemingly good JWT with no expiration"));
+        //Validation should be false when Expiration does not exist in an otherwise valid JWT
+        $decoded = BYUJWT::decode(static::$validJWT);
+        unset($decoded->exp);
+        $badJwt = JWT::encode($decoded, BYUJWT::getPublicKey());
+
+        $this->assertSame(false, BYUJWT::validateJWT($badJwt));
+        $this->assertInstanceOf('BYU\JWT\NoExpirationException', BYUJWT::$lastException);
     }
 
     public function testHeaderConstants()
@@ -95,52 +128,16 @@ final class BYUJWTTest extends TestCase
         $this->assertEquals(BYUJWT::BYU_JWT_HEADER_ORIGINAL, "X-JWT-Assertion-Original");
     }
 
-    public function testJTWVerifySuccesful()
+    public function testJTWValidateSuccesful()
     {
-        //Verify false when JWT Expiration does not exist
-        $this->assertSame(false, BYUJWT::verifyJWT("Good JWT", "Well-known URL here"));
-        $this->assertSame(false, BYUJWT::verifyJWT("Good JWT"));
+        $this->assertSame(true, BYUJWT::validateJWT(static::$validJWT));
     }
 
     public function testJTWDecodeSuccesful()
     {
-                $decodedJwt = BYUJWT::jwtDecoded("Good JWT", "Well-known UTL here");
-                $this->assertEquals("649019965", $decodedJwt->byu->client->byuId);
-                $this->assertEquals("CLIENT_SUBSCRIBER", $decodedJwt->byu->client->claim_source);
-                $this->assertEquals("adddrop", $decodedJwt->byu->client->netId);
-                $this->assertEquals("377228062", $decodedJwt->byu->client->personId);
-                $this->assertEquals("Add", $decodedJwt->byu->client->preferredFirstName);
-                $this->assertEquals(" ", $decodedJwt->byu->client->prefix);
-                $this->assertEquals("Add", $decodedJwt->byu->client->restOfName);
-                $this->assertEquals("Drop, Add", $decodedJwt->byu->client->sortName);
-                $this->assertEquals("adddrop", $decodedJwt->byu->client->subscriberNetId);
-                $this->assertEquals(" ", $decodedJwt->byu->client->suffix);
-                $this->assertEquals("Drop", $decodedJwt->byu->client->surname);
-                $this->assertEquals("L", $decodedJwt->byu->client->surnamePosition);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->byuId);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->netId);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->personId);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->preferredFirstName);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->prefix);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->restOfName);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->sortName);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->suffix);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->surname);
-                #$this->assertEquals("adddrop", $decodedJwt->byu->resourceOwner->surnamePosition);
-                $this->assertEquals("649019965", $decodedJwt->byu->webResCheck->byuId);
-                $this->assertEquals("adddrop", $decodedJwt->byu->webResCheck->netId);
-                $this->assertEquals("377228062", $decodedJwt->byu->webResCheck->personId);
-                $this->assertEquals("/echo/v1", $decodedJwt->wso2->apiContext);
-                $this->assertEquals("2085", $decodedJwt->wso2->application->id);
-                $this->assertEquals("DefaultApplication", $decodedJwt->wso2->application->name);
-                $this->assertEquals("Unlimited", $decodedJwt->wso2->application->tier);
-                $this->assertEquals("5gzLjMUcx7qut3MuSf9xr8GV2BAa", $decodedJwt->wso2->clientId);
-                $this->assertEquals("adddrop@carbon.super", $decodedJwt->wso2->endUser);
-                $this->assertEquals("-1234", $decodedJwt->wso2->endUserTenantId);
-                $this->assertEquals("PRODUCTION", $decodedJwt->wso2->keyType);
-                $this->assertEquals("BYU/adddrop", $decodedJwt->wso2->subscriber);
-                $this->assertEquals("Bronze", $decodedJwt->wso2->tier);
-                $this->assertEquals("APPLICATION", $decodedJwt->wso2->userType);
-                $this->assertEquals("v1", $decodedJwt->wso2->version);
+        $decodedJwt = BYUJWT::decode(static::$validJWT);
+        $this->assertNotEmpty($decodedJwt);
+        $this->assertEquals('https://api.byu.edu', $decodedJwt->iss);
+        $this->assertEquals(static::$credentials->client_id, $decodedJwt->{'http://wso2.org/claims/client_id'});
     }
 }
